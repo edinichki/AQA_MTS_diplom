6572d45510b7dd3fa217e757159f012a09acc758
edinichki <edinichki@gmail.com>
2024-04-23 17:51:32 +0500

    Add project files.

---
+++ AQA_MTS_diplom/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AQA_MTS_diplom
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
